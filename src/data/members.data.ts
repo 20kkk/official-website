dfd20a0dadf5c727c2d30690ebe8e515d65242ea
20kkk <87109536+20kkk@users.noreply.github.com>
2022-04-11 18:17:59 +0800

    Update members.data.ts

--- src/data/members.data.ts
+++ src/data/members.data.ts
@@ -133,7 +133,13 @@ const members: Member[] = [
     githubUrl: 'https://github.com/ZDHdong',
     blogUrl: 'https://www.cnblogs.com/Z-dong/',
   },
-
+  {
+    name: '20kkk', 
+    avatar: 'github:20kkk',
+    description: '做一个有办法的人',
+    githubUrl: 'https://github.com/20kkk',
+    blogUrl: 'https://20kkk.github.io/',
+  },
 ]
 .map(it => ({
   ...it,
